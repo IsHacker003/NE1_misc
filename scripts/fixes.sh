0c5922f7a2ef7e5dca2566053d58bd06841e3a4a
IsHacker <ishayu301@gmail.com>
2025-06-28 23:32:26 +0700

    Update copyright

--- scripts/fixes.sh
+++ scripts/fixes.sh
@@ -1,4 +1,17 @@
 #!/system/bin/sh 
+
+#               Copyright (C) 2025  IsHacker
+#
+#     This script is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This script is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
 # Disable MTK Engineering mode app to prevent crash messages. Will be re-enabled a few seconds later.
 pm disable com.mediatek.engineermode
 # Check if the magisk module is installed. If not installed, install it and reboot.
