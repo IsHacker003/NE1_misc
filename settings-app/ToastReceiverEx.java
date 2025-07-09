497f1dc1022a3258954e83f7dfec294e143cd079
IsHacker <ishayu301@gmail.com>
2025-07-09 21:52:40 +0530

    Update ToastReceiverEx.java

--- settings-app/ToastReceiverEx.java
+++ settings-app/ToastReceiverEx.java
@@ -8,6 +8,6 @@ import android.widget.Toast;
 public class ToastReceiverEx extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Done! Rebooting in 10 seconds.....", 1).show();
+        Toast.makeText(context, "Done! Rebooting in 10 seconds.....", Toast.LENGHT_LONG).show();
     }
 }
